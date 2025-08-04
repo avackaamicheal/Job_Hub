a18c3ee320d1010843515cb081c222e8d7631733
avackaamicheal <avackaamicheal@gmail.com>
2025-08-04 23:23:34 +0100

    Day-20 Custom Login and Registration System from scratch

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,18 @@
 <?php
 
-use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisteredUserController;
 
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
+
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [LoginController::class, 'create']);
+Route::post('/login', [LoginController::class, 'store']);
